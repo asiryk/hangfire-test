6902adc67a72788a3e751937bd996b3f8818483d
asiryk <asiryk.net@gmail.com>
2024-05-17 15:07:58 +0300

    connect mysql db

--- hangfire-test/Program.cs
+++ hangfire-test/Program.cs
@@ -1,8 +1,19 @@
+using Hangfire;
+using Hangfire.MySql;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddHangfire((sp, config) =>
+{
+    var connectionString = sp.GetRequiredService<IConfiguration>().GetConnectionString("DbConnection");
+    var mysqlStorage = new MySqlStorage(connectionString, new MySqlStorageOptions());
+    config.UseStorage(mysqlStorage);
+});
+builder.Services.AddHangfireServer();
 
 var app = builder.Build();
 
---
+++ hangfire-test/README.md
@@ -0,0 +1,20 @@
+## Run docker mysql db
+
+```shell
+docker run --name mysql-hangfire -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=hangfire_db -p 3306:3306 -d mysql:latest
+```
+
+## Run docker compose
+
+```shell
+docker-compose build
+docker-compose up -d
+```
+
+## MySql
+
+```shell
+mysql -u root -p
+USE hangfire_db;
+SHOW TABLES;
+```
