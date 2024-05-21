02871497f36229d65c0d2dee2bd212ce90cde238
asiryk <asiryk.net@gmail.com>
2024-05-21 14:55:11 +0300

    remove duplicate jobs

--- hangfire-test/controllers/JobController.cs
+++ hangfire-test/controllers/JobController.cs
@@ -25,7 +25,6 @@ public class JobController : ControllerBase
     public ActionResult CreateBackgroundJob()
     {
         logger.LogInformation("background job");
-        backgroundJobs.Enqueue(() => new HttpClient().GetAsync("http://localhost:3000/background"));
         backgroundJobs.Enqueue<TestJob>((x) => x.GetAsync("background"));
         return Ok();
     }
@@ -38,7 +37,6 @@ public class JobController : ControllerBase
         var scheduleDateTime = DateTime.UtcNow.AddSeconds(5);
         var dateTimeOffset = new DateTimeOffset(scheduleDateTime);
 
-        backgroundJobs.Schedule(() => new HttpClient().GetAsync("http://localhost:3000/scheduled"), dateTimeOffset);
         backgroundJobs.Schedule<TestJob>((x) => x.GetAsync("scheduled"), dateTimeOffset);
         return Ok();
     }
@@ -51,20 +49,11 @@ public class JobController : ControllerBase
         var scheduleDateTime = DateTime.UtcNow.AddSeconds(5);
         var dateTimeOffset = new DateTimeOffset(scheduleDateTime);
 
-        {
-            var job1 = backgroundJobs.Schedule(() => new HttpClient().GetAsync("http://localhost:3000/continuation/1"), dateTimeOffset);
-            var job2 = backgroundJobs.ContinueJobWith(job1, () => new HttpClient().GetAsync("http://localhost:3000/continuation/2"));
-            var job3 = backgroundJobs.ContinueJobWith(job2, () => new HttpClient().GetAsync("http://localhost:3000/continuation/3"));
-            var job4 = backgroundJobs.ContinueJobWith(job3, () => new HttpClient().GetAsync("http://localhost:3000/continuation/4"));
+        var job1 = backgroundJobs.Schedule<TestJob>((x) => x.GetAsync("continuation/1"), dateTimeOffset);
+        var job2 = backgroundJobs.ContinueJobWith<TestJob>(job1, (x) => x.GetAsync("continuation/2"));
+        var job3 = backgroundJobs.ContinueJobWith<TestJob>(job2, (x) => x.GetAsync("continuation/3"));
+        var job4 = backgroundJobs.ContinueJobWith<TestJob>(job3, (x) => x.GetAsync("continuation/4"));
 
-        }
-
-        {
-            var job1 = backgroundJobs.Schedule<TestJob>((x) => x.GetAsync("continuation/1"), dateTimeOffset);
-            var job2 = backgroundJobs.ContinueJobWith<TestJob>(job1, (x) => x.GetAsync("continuation/2"));
-            var job3 = backgroundJobs.ContinueJobWith<TestJob>(job2, (x) => x.GetAsync("continuation/3"));
-            var job4 = backgroundJobs.ContinueJobWith<TestJob>(job3, (x) => x.GetAsync("continuation/4"));
-        }
 
         return Ok();
     }
@@ -74,7 +63,6 @@ public class JobController : ControllerBase
     public ActionResult CreateRecurringJob()
     {
         logger.LogInformation("recurring job");
-        recurringJobs.AddOrUpdate("RecurringJob1", () => new HttpClient().GetAsync("http://localhost:3000/recurring"), "* * * * *");
         recurringJobs.AddOrUpdate<TestJob>("RecurringJob1TestJob", (x) => x.GetAsync("recurring"), "* * * * *");
         return Ok();
     }
